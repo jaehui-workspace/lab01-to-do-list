60eee0ac5ed364fec91f85c04712d01466872612
jaehui-workspace <kor_232@hotmail.com>
2024-02-11 11:50:37 -0700

    changed template

--- js/templates/toDoItem.js
+++ js/templates/toDoItem.js
@@ -9,7 +9,7 @@ import { toElement } from './../utils/toElement';
 function toDoItemTemplate(todo, status, category, start, finished) {
     const template = `
     <section class="my-2">
-        <ul class="max-w-xs flex flex-col">
+        <ul class=" max-w-md flex flex-col">
             <li class="inline-flex items-center gap-x-2 py-3 px-4 text-sm font-medium bg-white border border-gray-200 text-gray-800 -mt-px first:rounded-t-lg first:mt-0 last:rounded-b-lg dark:bg-slate-900 dark:border-gray-700 dark:text-white">
                 <b>To Do:</b> ${todo}
             </li>
